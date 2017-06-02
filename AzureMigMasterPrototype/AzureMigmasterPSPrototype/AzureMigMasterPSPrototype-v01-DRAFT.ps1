b2061654334d300f7ab7770d807c2dd01e02b3d1
styladj <matthias.kainz@gmail.com>
2017-06-02 18:19:59 +0200

    Draft vom Powershell Prototypen

--- AzureMigMasterPrototype/AzureMigmasterPSPrototype/AzureMigMasterPSPrototype-v01-DRAFT.ps1
+++ AzureMigMasterPrototype/AzureMigmasterPSPrototype/AzureMigMasterPSPrototype-v01-DRAFT.ps1
@@ -1,5 +1,8 @@
 ﻿#
 # Script.ps1
 # Script for automatic replication of pre-defined on-premises VMs
-# Script aims to connect to Azure, will enable replication for them (inside Azure) and finally will force 
-# the migration to Azure
+# Script aims to connect to Azure, will enable replication for them (inside Azure), should also do a test 
+# failover and finally will force the migration to Azure.
+
+# First of all, start to import necessary Azure PowerShell modules...
+Import-PSModule

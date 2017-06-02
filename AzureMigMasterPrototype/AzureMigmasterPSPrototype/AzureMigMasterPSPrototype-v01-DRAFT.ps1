1d7c39c5e89c9a0fdd17d9a74562bc222a42b088
styladj <matthias.kainz@gmail.com>
2017-06-02 18:13:40 +0200

    Kleine Änderungen - nur ein Testlauf

---
+++ AzureMigMasterPrototype/AzureMigmasterPSPrototype/AzureMigMasterPSPrototype-v01-DRAFT.ps1
@@ -0,0 +1,5 @@
+﻿#
+# Script.ps1
+# Script for automatic replication of pre-defined on-premises VMs
+# Script aims to connect to Azure, will enable replication for them (inside Azure) and finally will force 
+# the migration to Azure

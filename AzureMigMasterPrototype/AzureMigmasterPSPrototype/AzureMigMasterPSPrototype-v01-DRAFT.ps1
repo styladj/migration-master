110d0b22c1f7d4e708c93eedda0333aadd764dfb
styladj <matthias.kainz@gmail.com>
2017-06-10 16:23:55 +0200

    new draft release of PS Prototype
    + new draft release of PS Prototype
    + added ARMClient installation history
    + added ARMClient installation package
    + added session buddy export for ARMClient specific configuration

--- AzureMigMasterPrototype/AzureMigmasterPSPrototype/AzureMigMasterPSPrototype-v01-DRAFT.ps1
+++ AzureMigMasterPrototype/AzureMigmasterPSPrototype/AzureMigMasterPSPrototype-v01-DRAFT.ps1
@@ -4,5 +4,4 @@
 # Script aims to connect to Azure, will enable replication for them (inside Azure), should also do a test 
 # failover and finally will force the migration to Azure.
 
-# First of all, start to import necessary Azure PowerShell modules...
-Import-PSModule
+# First of all, locate ARMClient

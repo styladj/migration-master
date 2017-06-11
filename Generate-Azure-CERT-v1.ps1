2f087741528121db972d52e2dca7e3af3a7365f3
styladj <matthias.kainz@gmail.com>
2017-06-11 22:36:05 +0200

    test

--- Generate-Azure-CERT-v1.ps1
+++
@@ -1,4 +0,0 @@
-$cert = New-SelfSignedCertificate -DnsName mmaster.cloudapp.net -CertStoreLocation "cert:\LocalMachine\My"
-$password = ConvertTo-SecureString -String "V67OC!Mf" -Force -AsPlainText
-Export-PfxCertificate -Cert $cert -FilePath ".\Azure-CERT-v1.pfx" -Password $password
-Export-Certificate -Type CERT -Cert $cert -FilePath .\Azure-CERT-v1.cer

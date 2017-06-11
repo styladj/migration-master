d2bf61c1809979b4420c21bcd4f0d05d270289cc
styladj <matthias.kainz@gmail.com>
2017-06-11 16:32:12 +0200

    Draft vom Powershell Prototypen

--- AzureMigMasterPrototype/AzureMigmasterPSPrototype/AzureMigMasterPSPrototype-v01-DRAFT.ps1
+++ AzureMigMasterPrototype/AzureMigmasterPSPrototype/AzureMigMasterPSPrototype-v01-DRAFT.ps1
@@ -4,4 +4,16 @@
 # Script aims to connect to Azure, will enable replication for them (inside Azure), should also do a test 
 # failover and finally will force the migration to Azure.
 
-# First of all, locate ARMClient
+
+# First of all, adapt PowerShell Execution Policy
+Echo "PowerShell Execution Policy will be adapted now accordingly..."
+Set-ExecutionPolicy AllSigned
+
+# Now install 'chocolatey', the software management which will install the 'ARMClient' for us
+iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Use chocolatey to install 'ARMClient'
+choco install armclient
+
+# Generate test log
+echo "install" > test.log

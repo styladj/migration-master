29ab6f67a7c8d6dca73ae49c9050c8a661ddebdf
styladj <matthias.kainz@gmail.com>
2017-06-19 23:47:21 +0200

    test

--- AzureMigMasterPrototype/AzureMigmasterPSPrototype/AzureMigMasterPSPrototype-v01-DRAFT.ps1
+++ AzureMigMasterPrototype/AzureMigmasterPSPrototype/AzureMigMasterPSPrototype-v01-DRAFT.ps1
@@ -18,6 +18,7 @@
 #	- added resource group listing
 # 2017-06-12:
 #	- added fabrics
+#	- prepared prototype for replication
 ############################################################
 
 # BASIC VARIABLES
@@ -35,6 +36,18 @@ Set-Variable -Name "AppID" -Value "8c2e06a6-d5b6-4be1-8d60-7086d2ccc972";
 Set-Variable -Name "SPKey" -Value "IyvZvyZDIuuLRucg0Q+ssjACzacwJrU6k/3OZv1dVz0=";
 # VAR for Azure Site Recovery vault
 Set-Variable -Name "ASRVault" -Value "MMaster-AZ-Site-Recovery";
+# VAR for Azure Site Recovery fabric with friendlyName SUN; Azure requires the id;
+# id for Sun is bb725fe4ee3afccf122c02184fd8d596a21ea8f232b6cf782fb16525f58a529f;
+Set-Variable -Name "fabric" -Value "bb725fe4ee3afccf122c02184fd8d596a21ea8f232b6cf782fb16525f58a529f";
+# VAR for Recovery Services Providers id in Azure Site Recovery;
+Set-Variable -Name "ASRProviderID" -Value "6484e0e8-9d78-46e1-b1ac-278f76e0725d";
+# VAR for Protection container name of Configuration Server 'SUN';
+# Protection container name is in this case: cloud_6484e0e8-9d78-46e1-b1ac-278f76e0725d;
+Set-Variable -Name "ASRProtectionContainerName" -Value "cloud_6484e0e8-9d78-46e1-b1ac-278f76e0725d";
+
+# VARIABLES FOR ADVANCED PART
+# VAR for ASR name of protectable on-premises VM 'Moon_UbuntuBOX'
+Set-Variable -Name "ProtectableItemMoon" -Value "28cb61d5-4af6-11e7-8306-005056ae9154"
 
 # Return values to user
 Echo "Your tenant and application ID are: ";
@@ -74,4 +87,42 @@ armclient GET $SUB/resourceGroups?api-version=$ResAZapi
 
 # Get fabrics in Azure Site Recovery
 Echo "Getting list of fabrics in Azure Site Recovery..."
-armclient GET $SUB/resourceGroups/$ResGroup//providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics?api-version=2015-11-10
+armclient GET $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics?api-version=2015-11-10
+
+Echo "Getting details about our fabric..."
+armclient GET $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/"$fabric"?api-version=2015-11-10
+
+# Get recovery services providers in Azure Site Recovery
+Echo "Getting recovery services providers in Azure Site Recovery..."
+armclient GET $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/$fabric/replicationRecoveryServicesProviders?api-version=2015-11-10
+
+# Protection Container
+# First of all, list all protection containers in our replication fabric
+Echo "List all protection containers..."
+armclient GET $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/$fabric/replicationProtectionContainers/?api-version=2015-11-10
+
+##################### END OF BASIC PART ##################
+
+##################### VM PROTECTION ####################
+# Now the prototype will list all protectable items
+# This is the first critical step to successfully activate the replication for a VMware vSphere VM
+
+Echo "Now the prototype will list all protectable items..."
+Echo "ATTENTION: This is the first critical step to successfully activate the replication for a VMware vSphere VM!!!"
+Echo "Therefore the on-premises VMware vSphere infrastructure MUST BE accessible NOW!!"
+Echo "The prototype will now tell 'SUN' to retrieve information from the on-premises VMware vSphere Infrastructure."
+Echo "Proceed with ANY key when ready..."
+# Ask user for key input ;
+$HOST.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”) | OUT-NULL
+$HOST.UI.RawUI.Flushinputbuffer()
+# now retrieve protectable items from infrastructure ;
+armclient GET $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/$fabric/replicationProtectionContainers/$ASRProtectionContainerName/replicationProtectableItems?api-version=2015-11-10"&$filter=state eq '<protected | unprotected<'"
+
+# Get details of protectable on-premises VM 'Moon_UbuntuBOX' ;
+Echo "The prototype will now get more information about the protectable on-premises VM 'Moon_UbuntuBOX'..."
+armclient GET $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/$fabric/replicationProtectionContainers/$ASRProtectionContainerName/replicationProtectableItems/"$ProtectableItemMoon"?api-version=2015-11-10
+
+# Now add the protectable item to the protection container in Azure Site Recovery ;
+#Echo "The prototype will now add the protectable item 'Moon_UbuntuBOX' to the protection container in Azure Site Recovery..."
+#armclient POST $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/$fabric/replicationProtectionContainers/$ASRProtectionContainerName/discoverProtectableItem?api-version=2015-11-10
+

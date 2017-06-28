158d9279ed8152199881619d23c079c94598f4c0
styladj <matthias.kainz@gmail.com>
2017-06-28 21:51:31 +0200

    # 2017-06-28: # - added code for VM replication # - created JSON input file for VM to enable replication ä - added code to initiate repair VM replication state in ASR # - code cleanup

--- AzureMigMasterPrototype/AzureMigmasterPSPrototype/AzureMigMasterPSPrototype-v01.ps1
+++ AzureMigMasterPrototype/AzureMigmasterPSPrototype/AzureMigMasterPSPrototype-v01.ps1
@@ -4,11 +4,13 @@
 #############################################################
 #############################################################
 
-# Script for automatic replication of pre-defined on-premises VMs
+<# Script for automatic replication of pre-defined on-premises VMs
 # Script aims to connect to Azure, will enable replication for them (inside Azure), should also do a test 
 # failover and finally will force the migration to Azure.
-
-
+# And as an (optional) step: It can also initiate the repair of the replication of a specific VM
+# NOTE: THE PROTOTYPE WILL MODIFY YOUR INFRASTRUCTURE IN AZURE !!!
+# USE IT WITH CARE !!!	
+#
 ################# CHANGELOG #################################
 # v0.1 - 2017-05-30 initial release
 # 2017-06-11: 
@@ -18,106 +20,116 @@
 #	- added resource group listing
 # 2017-06-12:
 #	- added fabrics
-#	- prepared prototype for replication
 # 2017-06-25:
 #	- added test failover and failover commands
 #	- created JSON input files for test failover and final failover
+# 2017-06-28:
+#	- added code for VM replication
+#	- created JSON input file for VM to enable replication
+ä	- added code to initiate repair VM replication state in ASR
+#	- code cleanup
 ############################################################
+#>
 
-# BASIC VARIABLES
-# VAR for subscription
+Echo "Setting variables now..." 
+
+# BASIC VARIABLES ;
+# VAR for subscription ;
 Set-Variable -Name "SUB" -Value "/subscriptions/8074cfce-94b3-4af9-b6c3-18df7b96e869";
-# VAR for resourceGroup Name
+# VAR for resourceGroup Name ;
 Set-Variable -Name "ResGroup" -Value "mmaster";
-# VAR for Azure API version; change version here if required; 
+# VAR for Azure API version ;
+# change version here if required ;
 Set-Variable -Name "ResAZapi" -Value "2016-09-01";
-# VAR for Azure Tenant ID
+# VAR for Azure Tenant ID ;
 Set-Variable -Name "TenantID" -Value "e2367125-41e3-4124-bacc-5ee027d27287";
-# VAR for Azure AD application ID
+# VAR for Azure AD application ID ;
 Set-Variable -Name "AppID" -Value "8c2e06a6-d5b6-4be1-8d60-7086d2ccc972";
-# VAR for Azure AD application key
+# VAR for Azure AD application key ;
 Set-Variable -Name "SPKey" -Value "IyvZvyZDIuuLRucg0Q+ssjACzacwJrU6k/3OZv1dVz0=";
-# VAR for Azure Site Recovery vault
+# VAR for Azure Site Recovery vault ;
 Set-Variable -Name "ASRVault" -Value "MMaster-AZ-Site-Recovery";
-# VAR for Azure Site Recovery fabric with friendlyName SUN; Azure requires the id;
-# id for Sun is bb725fe4ee3afccf122c02184fd8d596a21ea8f232b6cf782fb16525f58a529f;
+# VAR for Azure Site Recovery fabric with friendlyName SUN; Azure requires the id ;
+# id for Sun is bb725fe4ee3afccf122c02184fd8d596a21ea8f232b6cf782fb16525f58a529f ;
 Set-Variable -Name "fabric" -Value "bb725fe4ee3afccf122c02184fd8d596a21ea8f232b6cf782fb16525f58a529f";
-# VAR for Recovery Services Providers id in Azure Site Recovery;
+# VAR for Recovery Services Providers id in Azure Site Recovery ;
 Set-Variable -Name "ASRProviderID" -Value "6484e0e8-9d78-46e1-b1ac-278f76e0725d";
-# VAR for Protection container name of Configuration Server 'SUN';
-# Protection container name is in this case: cloud_6484e0e8-9d78-46e1-b1ac-278f76e0725d;
+# VAR for Protection container name of Configuration Server 'SUN' ;
+# Protection container name is in this case: cloud_6484e0e8-9d78-46e1-b1ac-278f76e0725d ;
 Set-Variable -Name "ASRProtectionContainerName" -Value "cloud_6484e0e8-9d78-46e1-b1ac-278f76e0725d";
 
-# VARIABLES FOR ADVANCED PART
-# VAR for ASR name of protectable on-premises VM 'Moon_UbuntuBOX'
+# VARIABLES FOR ADVANCED PART ;
+# VAR for ASR name of protectable on-premises VM 'Moon_UbuntuBOX' ;
 Set-Variable -Name "ProtectableItemMoon" -Value "28cb61d5-4af6-11e7-8306-005056ae9154"
 
-# VAR for ASR name of protected on-premises VM 'Moon_UbuntuBOX'
+# VAR for ASR name of protected on-premises VM 'Moon_UbuntuBOX' ;
 Set-Variable -Name "ProtectedItemMoon" -Value "28cb61d5-4af6-11e7-8306-005056ae9154"
 
-# VAR for Replication policy name in ASR
+# VAR for Replication policy name in ASR ;
 Set-Variable -Name "ReplicationPolicy" -Value "d8e4aaf5-3fee-4291-8928-4aff1dc783cf"
 
-# VAR for Failback Replication policy name in ASR
+# VAR for Failback Replication policy name in ASR ;
 Set-Variable -Name "FailbackReplicationPolicy" -Value "10c93c82-a5ac-4a03-bc7f-2ad1943af72a"
 
-# Return values to user
+# Return values to user ;
 Echo "Your tenant and application ID are: ";
 Get-Variable -Name "TenantID"
 Get-Variable -Name "AppID"
 
-
-# Wait for User input to start application
+# Wait for User input to start application ;
+Echo "ATTENTION: THE PROTOTYPE WILL MODIFY YOUR INFRASTRUCTURE IN AZURE !!!"
+Echo "USE IT WITH CARE !!!"
 Echo "Hit any key to start the application..."
 $HOST.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”) | OUT-NULL
 $HOST.UI.RawUI.Flushinputbuffer()
 
-# LOG START
+# LOG START ;
 echo "Script started on: " > logs/script.log
 date >> logs/script.log
 
-# First of all, adapt PowerShell Execution Policy
+# First of all, adapt PowerShell Execution Policy ;
 Echo "PowerShell Execution Policy will be adapted now accordingly..."
 Set-ExecutionPolicy ByPass
 
-# Now install 'chocolatey', the software management which will install the 'ARMClient' for us
+# Now install 'chocolatey', the software management which will install the 'ARMClient' for us ;
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# Use chocolatey to install 'ARMClient'
+# Use chocolatey to install 'ARMClient' ;
 choco install armclient
 
-# Authenticate with Azure; Get a token
+# Authenticate with Azure; Get a token ;
+Echo "Requesting authentication token now by using Service Principal account..."
 armclient spn $TenantID $AppID $SPKey
 
 Echo "Hit any key to start basic tests to ensure you can access Azure..."
 $HOST.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”) | OUT-NULL
 $HOST.UI.RawUI.Flushinputbuffer()
 
-# Get resource groups in Azure  and display all available resource groups the service principal can access
+# Get resource groups in Azure  and display all available resource groups the service principal can access ;
 Echo "Getting and displaying all resource groups the service principal can access in Azure..."
 armclient GET $SUB/resourceGroups?api-version=$ResAZapi
 
-# Get fabrics in Azure Site Recovery
+# Get fabrics in Azure Site Recovery ;
 Echo "Getting list of fabrics in Azure Site Recovery..."
 armclient GET $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics?api-version=2015-11-10
 
 Echo "Getting details about our fabric..."
 armclient GET $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/"$fabric"?api-version=2015-11-10
 
-# Get recovery services providers in Azure Site Recovery
+# Get recovery services providers in Azure Site Recovery ;
 Echo "Getting recovery services providers in Azure Site Recovery..."
 armclient GET $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/$fabric/replicationRecoveryServicesProviders?api-version=2015-11-10
 
-# Protection Container
-# First of all, list all protection containers in our replication fabric
+# Protection Container ;
+# First of all, list all protection containers in our replication fabric ;
 Echo "List all protection containers..."
 armclient GET $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/$fabric/replicationProtectionContainers/?api-version=2015-11-10
 
 ##################### END OF BASIC PART ##################
 
 ##################### VM PROTECTION ####################
-# Now the prototype will list all protectable items
-# This is the first critical step to successfully activate the replication for a VMware vSphere VM
+# Now the prototype will list all protectable items ;
+# This is the first critical step to successfully activate the replication for a VMware vSphere VM ;
 
 Echo "Now the prototype will list all protectable items..."
 Echo "ATTENTION: This is the first critical step to successfully activate the replication for a VMware vSphere VM!!!"
@@ -145,13 +157,28 @@ armclient GET $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices
 Echo "The prototype will now get more information about the protected on-premises VM 'Moon_UbuntuBOX'..."
 armclient GET $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/$fabric/replicationProtectionContainers/$ASRProtectionContainerName/replicationProtectedItems/"$ProtectedItemMoon"?api-version=2015-11-10
 
+# Enable replication ;
+Echo "ATTENTION: The prototype will now try to enable the replication for the Client-VM!"
+Echo "Prototype will sleep for 10 hrs!"
+Echo "Proceed with ANY key when ready..."
+# Ask user for key input ;
+$HOST.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”) | OUT-NULL
+$HOST.UI.RawUI.Flushinputbuffer()
+
+# REST PUT to pass on the JSON file to enable the replication ;
+armclient PUT $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/$fabric/replicationProtectionContainers/$ASRProtection/ContainerName/replicationProtectedItems/$ProtectedItemMoon/testFailover?api-version=2015-11-10 "@.\json\ReplicationMoon.json"
+
+# Sleep and wait until replication is done ; 
+Echo "Entering sleep mode for 10 hrs!"
+Start-Sleep -Seconds 36000
+
 Echo "The prototype is now ready to execute the test failover!"
 Echo "Proceed with ANY key when ready..."
 # Ask user for key input ;
 $HOST.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”) | OUT-NULL
 $HOST.UI.RawUI.Flushinputbuffer()
 
-# Prepare for failover ...
+# Prepare for failover ... ;
 # Starting with testFailover ... ;
 armclient POST $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/$fabric/replicationProtectionContainers/$ASRProtection/ContainerName/replicationProtectedItems/$ProtectedItemMoon/testFailover?api-version=2015-11-10 "@.\json\TestFailoverMoon.json"
 Echo "Check the Azure Portal now and complete the test failover."
@@ -164,8 +191,20 @@ Echo "Proceed with ANY key when ready..."
 $HOST.UI.RawUI.ReadKey(“NoEcho,IncludeKeyDown”) | OUT-NULL
 $HOST.UI.RawUI.Flushinputbuffer()
 
-# Final failover ;
+# Final failover >
 Echo "The prototype will now execute the failover together with Azure Site Recovery."
 Echo "After the failover has been completed successfully you will be able to see the VM in 'Virtual Machines' on Azure."
 armclient POST $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/$fabric/replicationProtectionContainers/$ASRProtection/ContainerName/replicationProtectedItems/$ProtectedItemMoon/unPlannedFailover?api-version=2015-11-10 "@.\json\FinalFailoverMoon.json"
 
+<# Repair VM replication state in Azure Site Recovery ; 
+# Uncomment these lines only if you want to do this ;
+# EXAMPLE command from ASR REST API Documentation:
+# https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.RecoveryServices/vaults/{resourceName}/replicationFabrics/{fabricName}/replicationProtectionContainers/{containerName}/replicationProtectedItems/{protectedItemName}/resyncVM?api-version=<api-version> #>
+ 
+# Optional: initiate repair of the VM replication state
+# Echo "The prototype will now initiate the repair of the VM replication state!"
+# armclient POST $SUB/resourceGroups/$ResGroup/providers/Microsoft.RecoveryServices/vaults/$ASRVault/replicationFabrics/$fabric/replicationProtectionContainers/$ASRProtection/ContainerName/replicationProtectedItems/$ProtectedItemMoon/resyncVM?api-version=2015-11-10
+
+# LOG END ;
+echo "Script ended on: " > logs/script.log
+date >> logs/script.log
